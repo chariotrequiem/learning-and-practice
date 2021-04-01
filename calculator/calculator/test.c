eec02d4a9e755856baaf2b076e59c4ac7b67d8dd
chariotrequiem <2856793309@qq.com>
2021-04-01 16:27:17 +0800

    简易计算器（第一版）

---
+++ calculator/calculator/test.c
@@ -0,0 +1,121 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//函数1：显示界面函数
+//double calc()
+//{
+//	double num1 ,num2;
+//	char a;
+//	/*while (1)
+//	{*/
+//		printf("请输入第一个数: \n");
+//		scanf("%lf", &num1);
+//		printf("请输入运算符(+ - * /): \n");
+//		scanf("%c", &a);
+//		printf("请输入第二个数: \n");
+//		scanf("%lf", &num2);
+		/*swith(a)
+		{
+			case'+':
+				printf("%lf + %lf = %lf\n", num1, num2,  num1 + num2);
+				break;
+			case'-':
+				printf("%lf - %lf = %lf\n", num1, num2,  num1 - num2);
+				break;
+			case'*':
+				printf("%lf * %lf = %lf\n", num1, num2,  num1 * num2);
+				break;
+			case'/':
+				if (num2 == 0)
+				{
+					printf("除数不能为0.\n");
+				}
+				else
+				{
+					printf("%lf / %lf = %lf\n", num1, num2, num1 / num2);
+				}
+				break;
+			default:
+				break;
+		}*/
+//	/*}*/
+//
+//	return 0.0;
+//}
+//int main()
+//{
+//	menu();
+//	//calc();
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int a, b;
+//	char c;
+//	scanf("%d %c %d", &a, &c, &b);
+//	switch (c)                                         //不要写成c=getchar() 
+//	{
+//	case'+':printf("%d", a + b); break;              //不要在case后面打（） 
+//	case'-':printf("%d", a - b); break;
+//	case'*':printf("%d", a*b); break;
+//	case'/':if (b == 0)
+//		printf("除数不能为0！");
+//			else
+//				printf("%f", a*1.0 / b); break;          //注意乘的1.0 
+//	case'%':printf("%d", a%b); break;
+//	default:break;
+//	}
+//	return 0;
+//}
+//void menu()
+//{
+//	printf("简易计算器               \n");
+//	printf("\n");
+//}
+//double calc()
+//{
+//	double num1, num2;
+//	char a;
+//	while (1)
+//	{
+//		printf("请输入第一个数:\n");
+//		scanf("%lf", &num1);
+//		getchar();//吃掉\n
+//		printf("请输入运算符(+ - * /): \n");
+//		scanf("%c", &a);
+//		printf("请输入第二个数: \n");
+//		scanf("%lf", &num2);
+//		switch (a)
+//		{
+//		case'+':
+//			printf("%lf + %lf = %lf\n", num1, num2, num1 + num2);
+//			break;
+//		case'-':
+//			printf("%lf - %lf = %lf\n", num1, num2, num1 - num2);
+//			break;
+//		case'*':
+//			printf("%lf * %lf = %lf\n", num1, num2, num1 * num2);
+//			break;
+//		case'/':
+//			if (num2 == 0)
+//			{
+//				printf("除数不能为0\n");
+//			}
+//			else
+//			{
+//				printf("%lf / %lf = %lf\n", num1, num2, num1 / num2);
+//			}
+//			break;
+//		default:
+//			break;
+//		}
+//	}
+//	return 0.0;
+//}
+//int main()
+//{
+//	menu();
+//	calc();
+//	return 0;
+//}
+
