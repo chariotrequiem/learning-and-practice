b2361acdd69fbb0288fadc8f199954ea4c9302b3
chariotrequiem <2856793309@qq.com>
2021-04-16 17:00:38 +0800

    2021年4月16日学习

---
+++ test-4-16/test-4-16/test.c
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+
+//int main()
+//{
+//	int arr[10] = { 0 };
+//	int i = 0;
+//	for (i = 0; i < 10; i++)
+//	{
+//		arr[i] = i;
+//	}
+//	for (i = 0; i < 10; i++)
+//	{
+//		printf("%d ", arr[i]);
+//	}
+//	return 0;
+//}
+//翻译环境，在这个环境中，源代码被转换成可执行的代码
+//执行环境，它用于实际执行代码
+
