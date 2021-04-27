660f7c2f4f3cfad74061a93a9dde29d2eac871d9
chariotrequiem <2856793309@qq.com>
2021-04-27 21:13:02 +0800

    4.27

--- test_4_27/test_4_27/calculator.cpp
+++ test_4_27/test_4_27/calculator.cpp
@@ -257,10 +257,10 @@ public:
 		cout << "Animal构造函数调用" << endl;
 	}
 	//利用虚析构可以解决父类指针释放子类指针对象时不干净的问题
-	//virtual ~Animal()
-	//{
-	//	cout << "Animal析构函数调用" << endl;
-	//}
+	virtual ~Animal()
+	{
+		cout << "Animal析构函数调用" << endl;
+	}
 	//纯虚析构  需要声明也需要实现
 	//有了纯虚析构后，这个类页数与抽象类，无法实例化对象
 	virtual ~Animal() = 0;
@@ -485,23 +485,23 @@ void test01()
 	}
 	//4.读数据
 	//第一种
-	//char buf[1024] = { 0 };
-	//while (ifs >> buf)
-	//{
-	//	cout << buf << endl;
-	//}
+	char buf[1024] = { 0 };
+	while (ifs >> buf)
+	{
+		cout << buf << endl;
+	}
 	//第二种
-	//char buf[1024] = { 0 };
-	//while (ifs.getline(buf, sizeof(buf)))
-	//{
-	//	cout << buf << endl;
-	//}
+	char buf[1024] = { 0 };
+	while (ifs.getline(buf, sizeof(buf)))
+	{
+		cout << buf << endl;
+	}
 	//第三种
-	//string buf;
-	//while (getline(ifs, buf))
-	//{
-	//	cout << buf << endl;
-	//}
+	string buf;
+	while (getline(ifs, buf))
+	{
+		cout << buf << endl;
+	}
 	//第四种
 	char c;
 	while ((c = ifs.get()) != EOF)//EOF - end of file
